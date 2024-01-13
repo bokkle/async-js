eacf5f3d24d1c4fbfb10f95dd6aa9ced7f3dfa2f
bokkle <mitch.r_@hotmail.com>
2024-01-13 15:42:10 -0500

    .

--- challenges.js
+++ challenges.js
@@ -39,6 +39,25 @@ Test data:
 § Coordinates 3: -33.933, 18.474
 */
 
+const renderCountry = (data, className = '') => {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)}M</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  //   countriesContainer.style.opacity = 1;
+};
+
 const whereAmI = (lat, lng) => {
   fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
@@ -52,8 +71,9 @@ const whereAmI = (lat, lng) => {
     })
     .then((data) =>
       console.log(`You are in ${data.city}, ${data.countryName}.`)
-    )
+    ).then((kek) => renderCountry(kek))
     .catch((err) => console.error(`Error <3`));
 };
 console.log(whereAmI(52.508, 13.381));
-
+console.log(whereAmI(19.037, 72.873));
+console.log(whereAmI(-33.933, 18.474));
